e4d2fc86674130b507efe5a33406e3de9bb058c2
Eugene Monnier <v-emonnier@microsoft.com>
2024-10-31 16:02:45 -0700

    update counter function to use redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
 
   return (
     <div className="container text-center mt-5">
-      <h1>Counter Application</h1>
+      <h1>Counter Application with Redux</h1>
       <p className="display-4">{count}</p>
       <button
         className="btn btn-success mx-2"
-        onClick={() => setCount(count + 1)}
+        onClick={() => dispatch({ type: 'INCREMENT' })}
       >
           Increment
       </button>
       <button
         className="btn btn-danger mx-2"
-        onClick={() => setCount(count - 1)}
+        onClick={() => dispatch({ type: 'DECREMENT' })}
       >
           Decrement
       </button>

3e327579e79430bc4f94155fc96af35ef90e87e3
Eugene Monnier <v-emonnier@microsoft.com>
2024-11-04 10:35:45 -0800

    Set up React Router and define application routes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
+import Home from './Home';
 import TeamList from './TeamList';
 import Counter from './Counter';
 import './App.css';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar bg="primary" variant="dark" expand="lg">
         <Navbar.Brand href="#">Team Manager</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#">Home</Nav.Link>
-            <Nav.Link href="#">Team</Nav.Link>
-            <Nav.Link href="#">Projects</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/team">Team</Nav.Link>
+            <Nav.Link as={Link} to="/counter">Counter</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <div className="container text-center mt-5">
-        <h1>Counter Application with Redux</h1>
-        <Counter />
-        <TeamList />
+
+      <div className='app-constainer'>
+        <div className="container text-center mt-5">
+         <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/team" element={<TeamList />} />
+          <Route path="/counter" element={<Counter />} />
+         </Routes>
+        </div>
       </div>
-    </>
+    </Router>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+    return(
+        <div>
+            <h1>Welcome to the Team Manager App</h1>
+            <p>Use the navigation bar to view the team and counter.</p>
+        </div>
+    );
+}
+export default Home;
